38a5b0ba5c5a578e6893e8f447822890485a72ab
Vikas singh varma <Vikashverma3001@gmail.com>
2024-12-09 12:04:44 +0530

    error fix it

--- backend/db/dbConnection.js
+++ backend/db/dbConnection.js
@@ -4,7 +4,6 @@ const connectDb = async()=>{
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
-        console.log("mongoose res:", res)
     } catch (error) {
         console.error("Error connecting to database", error);
         process.exit(1);
