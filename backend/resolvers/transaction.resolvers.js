5b03aaa6ec9493b6821d2ac5c9d28c562b4341ca
Vikas singh varma <Vikashverma3001@gmail.com>
2024-12-10 15:43:28 +0530

    transaction resolvers added

--- backend/resolvers/transaction.resolvers.js
+++ backend/resolvers/transaction.resolvers.js
@@ -1,9 +1,32 @@
 import { transactions } from "../dummyData/data.js"
+import Transactions from "../models/transaction.model.js";
 const transactionResolver = {
     Query:{
-        transactions: ()=>{
-            return transactions
+        transactions: async(_, __, context)=>{
+            try {
+                const userId = await context.getUser()._id;
+                if(!userId){
+                    throw new Error("Unauthorized");
+                }
+                const transactions = await Transactions.find({userId: userId});
+                return transactions;
+            } catch (err) {
+                console.log("Error getting transactions", err);
+                throw new Error(err.message || "Error getting transactions");
+            }
+        },
+        transaction: async(_, {transactionId})=>{
+            try {
+                const transaction = await Transactions.findById(transactionId);
+                return transaction;
+            } catch (err) {
+                console.log("Error getting transaction", err);
+                throw new Error(err.message || "Error getting transaction");
+            }
         }
+
+        // TODO: add user Transaction relation
+            
     },
     Mutation:{}
 }
