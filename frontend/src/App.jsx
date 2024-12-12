ff6a5e86ccf224d9e3e3f399c155c6b5a9eaf8b8
Vikas singh varma <Vikashverma3001@gmail.com>
2024-12-12 11:32:50 +0530

    routes setup and svg added for notfound page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import {Route, Routes} from "react-router-dom"
 
+import HomePage from "./pages/HomePage.jsx"
+import LoginPage from "./pages/LoginPage.jsx"
+import SignUpPage from "./pages/SignUpPage.jsx"
+import TransactionPage from "./pages/TransactionPage.jsx"
+import NotFound from "./pages/NotFound.jsx"
+import Header from "./components/Header.jsx"
 function App() {
-  
+  const authUser = false
   return (
     <>
-     <h1 className="text-3xl font-bold underline text-red-400">
-      Hello world!
-    </h1>
+      {authUser && < Header/>}
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/login' element={<LoginPage />} />
+				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/transaction/:id' element={<TransactionPage />} />
+				<Route path='*' element={<NotFound />} />
+			</Routes>
     </>
   )
 }
